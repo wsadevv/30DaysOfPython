dfb811351e9e60898dbb43287c15994b57c393db
Willian <wsadevv@gmail.com>
2016-11-18 12:01:13 -0200

    Day 1
    Day 1 activities: variables, strings and numbers

---
+++ Day 1/day1.py
@@ -0,0 +1,21 @@
+#Day 1
+
+#strings
+#Ao declarar variaveis nao e necessario colocar o tipo ou var
+
+#points separate decimals
+myFloat = 3.1415
+myString ="this is a phrase"
+myAnotherString = " but this is not, this is a period"
+
+myNumberString = "234"
+print(myString)
+print(myFloat)
+print(myString + myAnotherString)
+print(myFloat + 1.0)
+#concatenates 234 ten times
+print(myNumberString * 10)
+#different results (without .0 it rounds the result)
+print(9/4)
+print(9.0/4)
+#crazy huh?
