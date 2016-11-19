aa1d44518d8ea02a296d97fc0f25b7d4c9c5c85f
wsabsi <williansantana.angola@gmail.com>
2016-11-19 12:34:11 -0200

    Day 2 Activities
    List, dictionaries and tuples

---
+++ Day 2/day2.py
@@ -0,0 +1,42 @@
+#LISTS
+
+abc = "Heel"
+print(abc)
+
+#list
+
+myList = ['some string', 'second thing',123,"test"]
+#add new item
+myList.append("new object")
+#remove last item
+
+print("Before pop: ",len(myList))
+
+#i can set the index at pop method
+myList.pop(0)
+print(myList)
+#list size
+print(len(myList))
+myList.append("Cooool")
+print(myList)
+print(myList[len(myList)-1])
+
+#DICTIONARIES and TUPLES
+
+#<K,V> -> keys and values
+
+myDictionary = {
+"abcd" : "First four letters",
+"1234"   : "First four numbers"}
+
+print(myDictionary["1234"])
+
+#TUPLES
+
+#tup = ("abc","abc")
+
+tup = (("abcd","abcd"),("more","more"),("yet","yet"))
+
+print(tup[0])
+
+
