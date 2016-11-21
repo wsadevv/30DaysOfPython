ed3ef8d9b88db299809e5bf0ba477c4264335bed
Willian <wsadevv@gmail.com>
2016-11-20 22:40:15 -0200

    Day 3 code: loops and conditions

---
+++ Day 3/day3.py
@@ -0,0 +1,23 @@
+myList = [1,2,3,4,5,6,7,8,9,10,11,12,12,"abc","cde","fgh"]
+
+for x in myList:
+	print(x)
+
+i=1;
+while i<=10 :
+	print("yeah")
+	i+=1
+
+#True,False #boolean
+
+a = 0
+b = 0
+
+if(a==b):
+	teste = True
+if(not a==b): #(! no java == not no python)
+	teste = False
+
+print("Resultado booleano: ",teste)
+
+print (6 == 6)
